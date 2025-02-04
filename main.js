21b180ff56136e38bb763987f0d20211be047fab
Mark Vecchione <187446994+mark-vecchione@users.noreply.github.com>
2025-02-04 13:54:25 -0500

    Create play move function

--- main.js
+++ main.js
@@ -28,20 +28,68 @@ const initializeVariables = (data) => {
 const box = document.querySelector(".box");
 
 
-//Set win conditions
-
-//Start the game the game
+//Start the game 
 
 const startGame = (data) => {
     //Pull variables
-    initializeVariables(data);
-    console.log(data);
+    initializeVariables(data);   
 
+    // Call event listeners
+
+    const gameBoardEventListener = (data) => {
+        document.querySelectorAll(".box").forEach(box =>{
+            box.addEventListener('click', (event) =>{
+                playMove(event.target,data);
+            })
+        })
+    };
+
+    gameBoardEventListener(data);
+
+};
+
+
+//play move
+
+const playMove = (box, data) => {
+    //check game over conditions, if true do nothing
+    if(data.gameOver) {
+        return;
+    }
     
+    // check if box has letter in it, if so do nothing
+    if(data.board[box.id] === "X" || data.board[box.id] === "O") {
+        return;
+    }
 
-    // Add event listeners to gameboard
+    //check for tie
+    if(data.round > 8){
+        return;
+    }
 
-}
+    //Adjust DOM for player move
+    data.board[box.id] = data.currentPlayer;
+    box.textContent = data.currentPlayer;
+    box.className = data.currentPlayer === "X" ? "box player 1" : "box player 2";
+
+    console.log(box,data);
+
+    //
+
+};
+
+//Set win conditions
+
+const winConditions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+]
 
 //Determine current player 
 
