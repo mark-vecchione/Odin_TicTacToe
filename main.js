d455260e155a5bd5c3684dd14411feb8735cd7be
Mark Vecchione <187446994+mark-vecchione@users.noreply.github.com>
2025-02-04 16:37:45 -0500

    Built function to change player and adjust dom with a win

--- main.js
+++ main.js
@@ -10,9 +10,20 @@ form.addEventListener('submit', (event) => {
     const data = Object.fromEntries(formData);
     document.querySelector(".modal-wrapper").setAttribute('hidden',true);
     console.log(data);
-    startGame(data);
-    
+    startGame(data);    
 })
+
+// set win conditions
+const winConditions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
 
 //Initialize object variables 
 
@@ -31,6 +42,8 @@ const box = document.querySelector(".box");
 //Start the game 
 
 const startGame = (data) => {
+    //Adjust for player1 name to start
+    adjustDom('displayTurn', `${data.player1Name}'s turn`);
     //Pull variables
     initializeVariables(data);   
 
@@ -71,29 +84,70 @@ const playMove = (box, data) => {
     data.board[box.id] = data.currentPlayer;
     box.textContent = data.currentPlayer;
     box.className = data.currentPlayer === "X" ? "box player 1" : "box player 2";
+    // increase the round 
+    data.round++;
 
-    console.log(box,data);
+    //check end conditions 
+    if(endConditions(data)) {
+        return;
+    }
 
-    //
-
+    // change the current player: DOM and data.currentPlayer
+    changePlayer(data);
 };
 
-//Set win conditions
+//Set end conditions
 
-const winConditions = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [6,4,2]
-]
+const endConditions = (data) => {
+    //win, tie, or game still on
+    if(checkWinner(data)) {
+        // adjust dom
+        let winnerName = data.currentPlayer === "X" ? data.player1Name : data.player2Name;
+        adjustDom('displayTurn', winnerName + " has won the game");
+        data.gameOver = true;
+        return true
+    } else if (data.round ===9) {
+        //adjust dom
+        adjustDom('displayTurn'), "It's a tie!";
+        data.gameOver = true;
+        return true
+    }
+    else {
+        return false
+    }
+};
 
-//Determine current player 
+// check winner
 
-//After each move, check win conditions, if not met, move to next player
+const checkWinner = (data) => {
+    let result = false;
+    winConditions.forEach(condition => {
+        if(
+            data.board[condition[0]] === data.board[condition[1]] && 
+            data.board[condition[1]] === data.board[condition[2]]
+        ) {
+            console.log("player has won");
+            data.gameOver = true;
+            result = true;
+        }
+    })
+    return result;
+}
+
+//adjust DOM for end conditions
+
+const adjustDom = (className, textContent) => {
+    const elem = document.querySelector(`.${className}`);
+    elem.textContent = textContent;
+};
+
+//Change current player function
+
+const changePlayer = (data) => {
+    data.currentPlayer = data.currentPlayer === "X" ? "O" : "X";
+    let displayTurnText = data.currentPlayer === "X" ? data.player1Name : data.player2Name;
+    adjustDom('displayTurn', `${displayTurnText}'s turn`);
+}
 
 
 
